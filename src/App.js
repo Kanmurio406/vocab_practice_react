ebb1b2193bef7abdd722ab27a789adc82f8abc76
Kuan-Yu Chou <jenny2311@gmail.com>
2023-03-08 22:43:39 +0800

    add answer boxes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 // API: https://www.wordsapi.com/ 每天超過2500字的話要錢，有音節，不確定能否一次打量打下來做成資料庫，另外自己編資料庫也較麻煩
 // API: https://dictionaryapi.dev/ 免費，有音檔，沒音節
 // 母音vowel: a,e,i,o,u
 // 子音consonant
-const levelFive = [
-  "ally",
-  "alongside",
-  "alter",
-  "alternate",
-  "amend",
-  "ample",
-  "analyst",
-  "anonymous",
-  "anticipate",
-  "antique"
-];
 
-const syllablesVocabs = [
-  {
-    id: 1,
-    word: "competition",
-    syllables: {
-      count: 4,
-      lists: ["com", "pe", "ti", "tion"]
-    },
-    ch: "競賽"
-  },
-  {
-    id: 2,
-    word: "anticipate",
-    syllables: {
-      count: 4,
-      lists: ["an", "ti", "ci", "pate"]
-    },
-    ch: "預期"
+function Answer (props) {
+  return (
+    <button className='answer'>{props.value}</button>
+  )
+}
+
+function Answers (props) {
+  const answers = props.syllableMode ? props.currentVocab.syllables.lists : props.currentVocab.word
+  let answerBoxes = [];
+
+  for (let i = 0; i < answers.length; i++) {
+    answerBoxes.push(<Answer key={i} value="   " />)
   }
-];
 
-function Answers(props) {
-  return (<div class="answers">
 
-  </div>)
+  return (<div className="answers">
+    {answerBoxes}
+  </div>);
 }
 
-function Options(props) {
-  return (<div class="options">
+function Options () {
 
-  </div>)
+
+    return (<div className="options">
+
+    </div>)
 }
 
-function Tools(props) {
-  return(<div class="tools">
-
+function Tools ()  {
+  return(<div className="tools-container">
+    <button>母音提示</button>
+    <button>子音提示</button>
+    <button>←</button>   
   </div>)
 }
 
@@ -70,9 +54,9 @@ class Vocabulary extends React.Component {
 
   render() {
     return (
-    <div>
-        <Answers />
-        <Options />
+    <div className='answering-container'>
+        <Answers currentVocab={this.props.currentVocab} syllableMode={this.props.syllableMode} />
+        <Options currentVocab={this.props.currentVocab} syllableMode={this.props.syllableMode} />
         <Tools />
     </div>
     );
@@ -85,9 +69,9 @@ class Controller extends React.Component {
     this.state = {
       currentVocabIndex: 0,
       syllableMode: true,
-      syllablesVocabs: [
+      vocabs: [
         {
-          id: 1,
+          id: 0,
           word: "competition",
           syllables: {
             count: 4,
@@ -96,40 +80,55 @@ class Controller extends React.Component {
           ch: "競賽"
         },
         {
-          id: 2,
+          id: 1,
           word: "anticipate",
           syllables: {
             count: 4,
             lists: ["an", "ti", "ci", "pate"]
           },
           ch: "預期"
+        },
+        {
+          id: 2,
+          word: "alongside",
+          syllables: {
+            count: 2,
+            lists: ["along", "side"]
+          },
+          ch: "沿著"
+        },
+        {
+          id: 3,
+          word: "alternate",
+          syllables: {
+            count: 3,
+            lists: ["al", "ter", "nate"]
+          },
+          ch: "替代"
+        },
+        {
+          id: 4,
+          word: "anonymous",
+          syllables: {
+            count: 4,
+            lists: ["a", "no", "ny", "mous"]
+          },
+          ch: "匿名"
         }
-      ],
-      vocabs: [
-        "ally",
-        "alongside",
-        "alter",
-        "alternate",
-        "amend",
-        "ample",
-        "analyst",
-        "anonymous",
-        "anticipate",
-        "antique"
       ],
     }
   }
 
 
   render() {
-    const currentVocabList = this.state.syllableMode ? this.state.syllablesVocabs : this.state.vocabs;
+    const currentVocabList = this.state.vocabs;
     const currentVocab = currentVocabList[this.state.currentVocabIndex];
 
-
     return (
-      <div>
-        <button>播放聲音</button>
-        <Vocabulary vocab={currentVocab} />
+      <div className="container">
+        <h1>Simple English Vocabulary Dictation and Phonics Practice</h1>
+        <button className="play-sound">播放聲音</button>
+        <Vocabulary currentVocab={currentVocab} syllableMode={this.state.syllableMode} />
       </div>
     );
   }
