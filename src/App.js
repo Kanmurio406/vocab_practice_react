f33c2dee31efb209872d6da5f6afee5c10b30d68
Kuan-Yu Chou <jenny2311@gmail.com>
2023-03-14 21:29:08 +0800

    add basic draggable settings and answer check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 // API: https://www.wordsapi.com/ 每天超過2500字的話要錢，有音節，不確定能否一次打量打下來做成資料庫，另外自己編資料庫也較麻煩
@@ -6,44 +6,40 @@ import './App.scss';
 // 母音vowel: a,e,i,o,u
 // 子音consonant
 
+// 可拖曳的選項
+function DraggableItem(props) {
+  const handleDragStart = (event) => {
+    event.dataTransfer.setData('text/plain', props.value);
+    event.target.style.cursor = 'grabbing';
+  };
 
-// 控制提示按鈕的顯示 點擊後的回應
-// 按鈕關閉和開啟 一次只能用一種
-function Answer (props) {
+  const handleDragEnd = (event) => {
+    event.target.style.cursor = 'grab';
+  };
+
   return (
-    <button className='answer'>{props.value}</button>
-  )
+    <div
+      draggable="true"
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      style={{ cursor: 'grab', userSelect: 'none' }}
+      className='option'
+    >
+      {props.value}
+    </div>
+  );
 }
-
-function Answers (props) {
-  const answers = props.answers
-  let answerBoxes = [];
-
-  for (let i = 0; i < answers.length; i++) {
-    answerBoxes.push(<Answer key={i} value="   " />)
-  }
-
-  return (<div className="answers">
-    {answerBoxes}
-  </div>);
-}
-
 
 // 控制選項區塊的顯示 點擊後的回應
 // 點選選項 取消選擇
 // 按鈕關閉和開啟
-function Option(props) {
-  return (
-    <button className='option'>{props.value}</button>
-  )
-}
 
-function Options (props) {
+function Options(props) {
   const options = props.answers
   let optionBoxes = [];
-  
-  for (let i = 0; i < options.length; i++) { 
-    optionBoxes.push(<Option key={i} value={options[i]} />)
+
+  for (let i = 0; i < options.length; i++) {
+    optionBoxes.push(<DraggableItem key={i} value={options[i]} />)
   }
 
   function compareRandom(a, b) {
@@ -58,6 +54,48 @@ function Options (props) {
   </div>)
 }
 
+// 擺放答案的區域
+function DropZone(props) {
+
+  const [droppedItem, setDroppedItem] = useState('');
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData('text/plain');
+    setDroppedItem(data);
+    props.handleResponse(data, props.index);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      className='answer'
+    >
+      {droppedItem ? props.response : "   "}
+    </div>
+  );
+}
+
+// 控制提示按鈕的顯示 點擊後的回應
+// 按鈕關閉和開啟 一次只能用一種
+function Answers (props) {
+  const answers = props.answers
+  let answerBoxes = [];
+
+  for (let i = 0; i < answers.length; i++) {
+    answerBoxes.push(<DropZone key={i} index={i} response={props.responses[i]} handleResponse={props.handleResponse} checkAnswer={props.checkAnswer} />)
+  }
+
+  return (<div className="answers">
+    {answerBoxes}
+  </div>);
+}
+
 
 // 控制提示按鈕的顯示 點擊後的回應
 // 按鈕關閉和開啟 一次只能用一種
@@ -65,31 +103,55 @@ function Tools ()  {
   return(<div className="tools-container">
     <button>母音提示</button>
     <button>子音提示</button>
-    <button>←</button>   
+    {/* <button>←</button>    */}
   </div>)
 }
 
 
 // 控制單一題的答案 確認答案正確與否 聲音播放 中文顯示
 class Vocabulary extends React.Component {
-  // renderSquare(i) {
-  //   return (
-  //       <Square 
-  //         value={this.props.squares[i]}
-  //         onClick={() => this.props.onClick(i)}
-  //       />
-  //   );
-  // }
-
   // 應該在這層就確認是哪一種 mode
+  constructor (props) {
+    super(props);
+    this.state = {
+      responses: Array(4).fill(null)
+    }
+  }
 
   render() {
     const currentVocab = this.props.currentVocab
     const answers = this.props.syllableMode ? currentVocab.syllables.lists : currentVocab.word
+    const answerString = this.props.syllableMode ? answers.join("") : answers
+
+    const handleResponse =  (data, index) => {
+      let newResponses = [...this.state.responses];
+      newResponses[index] = data
+      this.setState({
+        responses: newResponses
+      }, () => {
+        checkAnswer()
+      })
+    }
+
+    const checkAnswer = () => {
+      if (this.state.responses.some((response => response === null))) return
+      let responseString = this.state.responses.join("")
+      if (responseString === answerString) {
+        // 答案正確
+        // 顯示通知
+        // 往下一題
+        console.log("correct!")
+      } else {
+        // 答案錯誤
+        // 顯示通知
+        // 重新答這題
+        console.log("wrong!")
+      }
+    }
 
     return (
     <div className='answering-container'>
-        <Answers answers={answers} />
+        <Answers answers={answers} responses={this.state.responses} handleResponse={handleResponse} checkAnswer={checkAnswer} />
         <Options answers={answers} />
         <Tools />
     </div>
